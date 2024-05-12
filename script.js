0c35303d0163adb7d0afbd0e090e3cebb833c9a1
Kookylo <154103116+Kookylo@users.noreply.github.com>
2024-05-11 21:42:02 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// IMAGE SLIDER
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider(){
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+}
+
+/**
+ * Displays the slide at the specified index.
+ * If the index is out of range, it wraps around to the first or last slide accordingly.
+ *
+ * @param {number} index - The index of the slide to be displayed.
+ */
+function showSlide(index){
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
